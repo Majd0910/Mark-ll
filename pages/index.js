732e0c25191c272b0599879da89c1cbd8acec25e
Majd0910 <157950344+Majd0910@users.noreply.github.com>
2026-08-19 21:20:11 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const AriaVoiceHud = dynamic(() => import("../components/AriaVoiceHud"), {
-  ssr: false,
-});
-
-export default function Home() {
-  return <AriaVoiceHud />;
-}
